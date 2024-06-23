7bdad9c9ef07498f3a4a85bbf0e5ed8ca41454d7
Ahmed Bensalem <ahmed.ben.salemy@gmail.com>
2024-06-23 12:53:11 +0100

    Final commit - Finished interface with add & delete functions

--- frontend/src/components/ItemDetails.js
+++ frontend/src/components/ItemDetails.js
@@ -1,4 +1,20 @@
+import { useItemsContext } from "../useItemsContext";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
 const ItemDetails = ({ item }) => {
+  const { dispatch } = useItemsContext();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/items/" + item._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_ITEM", payload: json });
+    }
+  };
+
   return (
     <div className="item-details">
       <h4>{item.title}</h4>
@@ -6,7 +22,12 @@ const ItemDetails = ({ item }) => {
         <strong>Number: </strong>
         {item.number}
       </p>
-      <p>{item.createdAt}</p>
+      <p>
+        {formatDistanceToNow(new Date(item.createdAt), { addSuffix: true })}
+      </p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        delete
+      </span>
     </div>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ItemsContextProvider } from "./context/ItemsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ItemsContextProvider>
+      <App />
+    </ItemsContextProvider>
   </React.StrictMode>
 );
